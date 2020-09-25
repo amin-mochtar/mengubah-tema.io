b5d5fa02c4d15a1b0e69fb0c257aefefaeb0eae8
aminmochtarclear <aminmr.mochtar0123@gmail.com>
2020-09-25 08:44:28 +0700

    finalProjeksederhana

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const themeOption = document.getElementById("themeOption")
+const themeStylesheet = document.getElementById("themeStylesheet")
+
+themeOption.addEventListener("change", function () {
+    themeStylesheet.setAttribute("href", this.value + ".css")
+})
+
+
+// function myFunction() {
+//     alert("Hello friend, Tema Telah Berubah");
+// }
+// function isFunction() {
+//     alert("Hello friend, Tema kembali Default");
+// }
